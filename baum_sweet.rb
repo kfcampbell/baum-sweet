b57ec7677daf35410bab11daef5d4bec2b04201b
Keegan Campbell <Keegan.Campbell@alaskaair.com>
2018-01-23 09:47:22 -0800

    After initial refactor.

--- baum_sweet.rb
+++ baum_sweet.rb
@@ -7,28 +7,25 @@
 # ex output: [1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0]
 
 class BaumSweet
-    attr_accessor :given_num, :sequence
 
-    def initialize
-        @given_num = 0
-        @sequence = []
+    def baum_sweet(num)
+        if num > 0 
+            puts "Generating Baum-Sweet sequence of #{num}."
+            self.generate_sequence(num)
+        else
+            puts "#{num} is an invalid number to generate sequence."
+        end
     end
 
-    def start_game
-        puts "Input an integer."
-        @given_num = 20 # gets.to_i doesn't work in vscode so leave out for debugging purposes for now
-        self.start_procedure unless @given_num < 1
-    end
-
-    def start_procedure
+    def generate_sequence(num)
+        sequence = []
         i = 0
-        while i <= @given_num
+        while i <= num
             curr_bin_num = self.generate_binary_representation(i)
-            curr_block_sizes = self.get_block_sizes(curr_bin_num)
-            @sequence << self.has_odd_zero_block_size(curr_block_sizes)
+            sequence << self.contains_odd_zero_block(curr_bin_num)
             i += 1
         end
-        puts "sequence for #{@given_num}: #{@sequence}"
+        puts "sequence for #{num}: #{sequence}"
     end
 
     def generate_binary_representation(num)
@@ -46,46 +43,20 @@ class BaumSweet
             temp = temp / 2
         end
         while stack.count > 0
-            temp = stack.pop
-            bin.push(temp % 2)
+            bin.push(stack.pop % 2)
         end
         return bin
     end
 
-    def get_block_sizes(binary_num)
-        i = 0
-        block_sizes = []
-        while i < binary_num.count
-            if binary_num[i] == 0
-                block_size = 1
-                j = i + 1
-                while j < binary_num.count
-                    if binary_num[j] == 0
-                        block_size += 1
-                        j += 1
-                    else
-                        break
-                    end
-                end
-            block_sizes.push(block_size)
-            i = j
-            end
-            i += 1
+    def contains_odd_zero_block(binary_num)
+        query = /(?<!0)(00)*0(?!0)/
+        num = binary_num.join("")
+        matches = query.match(num)
+        if matches
+            return 0
         end
-        return block_sizes
-    end
-
-    def has_odd_zero_block_size(block_sizes)
-        i = 0
-        while i < block_sizes.count
-            if block_sizes[i] % 2 != 0 # odd length found
-                return 0
-            end
-            i += 1
-        end
-        return 1 
+        return 1
     end
 end
 
-game = BaumSweet.new
-game.start_game
+game = BaumSweet.new.baum_sweet(20)
